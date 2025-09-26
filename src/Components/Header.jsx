486b5e7deeaf46d22a9ecbfb6bbddfedfe0be759
PradeepTeppala <pradeepteppala1212@gmail.com>
2025-09-26 17:56:57 +0530

    Update Header Content

---
+++ src/Components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <div className="header-container">
+        <div className="header-content">
+          <div className="title">
+            <h1>
+              FilmIndustry.com
+            </h1>
+          </div>
+          <div className="header-menu">
+            <p>Home</p>
+            <p>Industry</p>
+            <p>Movies</p>
+            <p>Webseries</p>
+            <img src="/src/Components/Assets/user.png" alt="Account" />
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
